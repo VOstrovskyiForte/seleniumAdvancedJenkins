487b962a4597812734422bb3168abcdbef527f34
VOstrovskyiForte <volodymyr.ostrovskyi.forte@gmail.com>
2019-01-21 21:24:58 +0200

    no message

---
+++ SeleniumAdvanced-second-lection/SeleniumAdvanced-second-lection/Framework.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumAdvanced_second_lection
+{
+    public static class Framework
+    {
+
+
+        //It verifies whether it is the bottom by calculating the position
+        // Does not work normally without Thread.Sleep
+        public static void ScrollToBottomByPosition(this IJavaScriptExecutor executor)
+        {
+            bool isAtBottom = false;
+
+            while (!isAtBottom)
+            {
+                executor.ExecuteScript(@"window.scrollTo(0, document.querySelector('body').scrollHeight)");
+                int currentPosition = Convert.ToInt32(executor.ExecuteScript("return document.documentElement.scrollTop"));
+                int windowVisiblePartHeight = Convert.ToInt32(executor.ExecuteScript("return window.innerHeight"));
+                int fullDocumentHeight = Convert.ToInt32(executor.ExecuteScript("return document.documentElement.scrollHeight"));
+                Thread.Sleep(500);
+                if (currentPosition + windowVisiblePartHeight == fullDocumentHeight)
+                {
+                    isAtBottom = true;
+                    break;
+                }
+            }
+        }
+
+        //Verifies whether it is bottom of page by searching for "Make something awesome"
+        // label. It works without Thread.Sleep but depends on existing of this label
+        public static void ScrollToBottomByFindingElement(this IJavaScriptExecutor executor, IWebDriver driver)
+        {
+            bool isBottomButtonVisible = false;
+
+            while (!isBottomButtonVisible)
+            {
+                executor.ExecuteScript(@"window.scrollTo(0, document.querySelector('body').scrollHeight)");
+                if (driver.FindElements(By.XPath("//p[text()='Make something awesome']")).Count != 0)
+                {
+                    isBottomButtonVisible = true;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ SeleniumAdvanced-second-lection/SeleniumAdvanced-second-lection/TestSuite.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumAdvanced_second_lection
+{
+    [TestFixture]
+    class TestSuite
+    {
+        private IWebDriver driver;
+        private IJavaScriptExecutor executor;
+        private string downloadPath;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            ChromeOptions options = new ChromeOptions();
+            downloadPath = Path.Combine(@"C:\", "SeleniumDownloads", DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss"));
+            options.AddUserProfilePreference("download.default_directory", downloadPath);
+            options.AddArgument("--start-fullscreen");
+            driver = new ChromeDriver(options);
+            
+        }
+
+        [Test]
+        [Ignore("not used in homework")]
+        //[Category("FirstTest")]
+        public void SeleniumAdvancedSecondLectionTest()
+        {
+            //int a;
+
+            driver.Navigate().GoToUrl(@"https://unsplash.com/search/photos/test");
+
+            executor = (IJavaScriptExecutor)driver;
+
+            //To scroll to the bottom of page,
+            //can use any of these two methods by uncommenting one of them, 
+            // details in Framework.cs :
+
+            executor.ScrollToBottomByFindingElement(driver);
+            //executor.ScrollToBottomByPosition();
+
+            var downloadPhotoButtons = driver.FindElements(By.XPath("//a[@title='Download photo']/span"));
+
+            //Search for the lowest download button by comparing their positions
+            IWebElement lastDownloadPhotoButton = null;
+            int lastDownloadPhotoButtonPosition = 0;
+
+            foreach (IWebElement element in downloadPhotoButtons)
+            {
+                if (element.Location.Y > lastDownloadPhotoButtonPosition)
+                {
+                    lastDownloadPhotoButton = element;
+                    lastDownloadPhotoButtonPosition = element.Location.Y;
+                }
+            }
+
+            if (lastDownloadPhotoButton != null)
+                executor.ExecuteScript($"arguments[0].click()", lastDownloadPhotoButton);
+            else
+                throw new Exception("Last photo download button is not found");
+
+            //Wait until file is downloaded
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(p => (Directory.Exists(@downloadPath) && (Directory.GetFiles(downloadPath).Length > 0)));
+
+            int numberOfFilesInCurrentDownloadFolder = Directory.GetFiles(@downloadPath).Length;
+            Assert.That(numberOfFilesInCurrentDownloadFolder, Is.EqualTo(1));
+
+        }
+
+        [Test]
+        [Category("FirstTest")]
+        public void FirstTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        [Category("SecondTest")]
+        public void SecondTest()
+        {
+            Assert.IsTrue(1 == 2);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Dispose();
+        }
+    }
+}
